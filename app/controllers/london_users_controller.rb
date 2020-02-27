ac93fc2211fe2779807fb2cc37ef9f28032fc90f
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-02-27 23:01:00 +0000

    contollers changed

--- app/controllers/london_users_controller.rb
+++ app/controllers/london_users_controller.rb
@@ -1,8 +1,2 @@
 class LondonUsersController < ApplicationController
-
-    # def index
-    #     @london_users = LondonUser.all
-    #     render json: @london_users, except: [:created_at, :updated_at]
-    # end
-
 end

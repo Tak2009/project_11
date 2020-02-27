af0508df517a09190b669aee94fad4ea615a9ad7
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-02-27 21:19:33 +0000

    rails g resource user

---
+++ db/migrate/20200227211820_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.integer :user_id
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :ip_address
+      t.float :latitude
+      t.float :longitude
+      t.references :city, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

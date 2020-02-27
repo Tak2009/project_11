0638bbd1ce6fbd13ba0738218ccb60ebac9c1ee1
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-02-27 22:32:13 +0000

    routes changed and CORS

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -5,4 +5,14 @@ class CitiesController < ApplicationController
         render json: @cities, except: [:created_at, :updated_at]
     end
 
+    def london_users_index
+        @london_users = LondonUser.all
+        render json: @london_users, except: [:created_at, :updated_at]
+    end
+
+    def london_50_mi_of_london_users_index
+        @users = User.all
+        render json: @users, except: [:created_at, :updated_at]
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :london_users
-  resources :users
-  resources :cities
+  # resources :london_users
+  # resources :users
+  # resources :cities
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/cities", to:"cities#index", as:"cities"
+  get "/cities/london/users", to:"cities#london_users_index", as:"users_in_london"
+  get "/cities/within_50_mi_of_london/users", to:"cities#london_50_mi_of_london_users_index", as:"users_within_50_london"
+
 end

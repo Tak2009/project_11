43ebd9d954bbc21db4d3cf8bad215f06a6afa9a3
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-02-28 15:07:52 +0000

    testing

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,18 +1,20 @@
 class CitiesController < ApplicationController
 
     def index
-        @cities = City.all
-        render json: @cities, except: [:created_at, :updated_at]
+        cities = City.all
+        render json: cities, except: [:created_at, :updated_at]
+
     end
 
     def london_users_index
-        @london_users = LondonUser.all
-        render json: @london_users, except: [:created_at, :updated_at]
+        london_users = User.all.where(city_id: 1)
+        # render json: london_users, except: [:created_at, :updated_at], include: [:city]
+        render json: london_users.to_json(include: [:city], except: [:created_at, :updated_at])
     end
 
     def london_50_mi_of_london_users_index
-        @users = User.all
-        render json: @users, except: [:created_at, :updated_at]
+        users = User.all.all.where(city_id: 2)
+        render json: users, except: [:created_at, :updated_at], include: [:city]
     end
 
 end
